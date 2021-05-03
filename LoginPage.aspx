777fe933f69a826f62e1ceeb4aa0cae53edce6c8
Surekha546 <83571550+Surekha546@users.noreply.github.com>
2021-05-03 12:56:09 +0530

    Assignment Purpose

---
+++ LoginPage.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginPage.aspx.cs" Inherits="LoginForm.LoginPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-bottom: 23px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Name</td>
+                    <td><asp:TextBox ID="Name" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>Movie</td>
+                    <td>
+                        <asp:TextBox ID="Movie" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Review</td>
+                    <td><asp:TextBox ID="Review" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>Comment</td>
+                    <td><asp:TextBox ID="Comment" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                   <td colspan="2" align="center">
+                       <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click"></asp:Button>
+                       <asp:Button ID="Button2" runat="server" Text="Delete" OnClick="Button2_Click"></asp:Button>
+                       
+                      
+
+                <asp:GridView ID="GridView" runat ="server" CssClass="auto-style1"></asp:GridView>
+                       
+                      
+
+                   </td>
+                        
+                </tr>
+
+            </table>
+
+            <tr>
+            </tr>
+        </div>
+    </form>
+</body>
+</html>
---
+++ LoginPage.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LoginForm
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        readonly SqlConnection con = new SqlConnection(@" Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=[DataDirectory]\Database.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State==ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+            disp_data();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into Table values('"+ Name.Text +"','"+ Movie.Text +"','"+ Review.Text +"','"+ Comment +"')";
+            _=cmd.ExecuteNonQuery();
+
+            Name.Text = " ";
+            Movie.Text = " ";
+            Review.Text = " ";
+            Comment.Text = " ";
+
+            disp_data();
+        }
+        public void disp_data()
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Select * from Table";
+            _=cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView.DataSource = dt;
+            GridView.DataBind();
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from Table where Name='" + Name.Text + "'";
+            _ = cmd.ExecuteNonQuery();
+
+            Name.Text = " ";
+            disp_data();
+        }
+    }
+}
